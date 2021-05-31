b4bd5627aca3254ff6dfb3fab08d6bea644bad36
Devin Harris <dharris@cincom.com>
2021-05-30 23:49:26 -0400

    added fonts and not found route

--- public/src/router/index.ts
+++ public/src/router/index.ts
@@ -1,11 +1,24 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import routeNames from './routeNames'
 import Home from '../views/Home/index.vue'
+import NotFound from '../views/NotFound/index.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
+    name: routeNames.Home,
     component: Home
+  },
+  {
+    path: '/not-found',
+    name: routeNames.NotFound,
+    component: NotFound
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: {
+      name: routeNames.NotFound
+    }
   }
 ]
 
---
+++ public/src/views/NotFound/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="not-found">
+    <h1>Not Found</h1>
+  </div>
+</template>
+
+<script src="./not-found.ts"></script>
+<style src="./not-found.scss" lang="scss"></style>
