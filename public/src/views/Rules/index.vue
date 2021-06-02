b5cad4c53af15320b108fc742fb4e6228d401f51
Chris Bridewell <cjbaseball5@gmail.com>
2021-06-02 13:02:50 -0400

    Rules page

--- public/src/views/Rules/index.vue
+++ public/src/views/Rules/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="rules-container">
-    <h1>Rules</h1>
+    <h1 class="site-title">Rules</h1>
+    <div class="contact-card">
+      <h2>League Rules</h2>
+      <p>You can get in contact with us through the following things:</p>
+      <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+      </ul>
+    </div>
+    <div class="contact-card">
+      <h2>Gameplay Rules</h2>
+      <p>You can get in contact with us through the following things:</p>
+      <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+      </ul>
+    </div>
   </div>
 </template>
 
