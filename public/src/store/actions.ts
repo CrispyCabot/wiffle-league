1016fc72f1475bbdd5372632ed38d985727ffc92
Devin Harris <dharris@cincom.com>
2021-06-02 12:45:16 -0400

    style fixes

--- public/src/store/actions.ts
+++ public/src/store/actions.ts
@@ -1,15 +1,15 @@
 import api from '@/api/api'
 
 export default {
-    fetchLeagues() {
-      return new Promise((resolve, reject) => {
-        api.get('/leagues')
-          .then(({data}) => {
-            resolve(data)
-          })
-          .catch((error) => {
-            reject(error)
-          })
-      })
-    }
+  fetchLeagues() {
+    return new Promise((resolve, reject) => {
+      api.get('/leagues')
+        .then(({data}) => {
+          resolve(data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  }
 }
