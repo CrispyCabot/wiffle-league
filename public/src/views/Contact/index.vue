b0be6a5894cebdcd7842f4835b7067b8493bcb59
Chris Bridewell <cjbaseball5@gmail.com>
2021-06-02 12:59:55 -0400

    Contact page

--- public/src/views/Contact/index.vue
+++ public/src/views/Contact/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="contact-container">
-    <h1>Contact</h1>
+    <h1 class="site-title">Contact</h1>
+    <div class="contact-card">
+      <h2>Questions? Comments? Concerns? Something Else?</h2>
+      <p>You can get in contact with us through the following things:</p>
+      <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+      </ul>
+    </div>
   </div>
 </template>
 
