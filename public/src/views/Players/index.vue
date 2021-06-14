6fc4c22f808ac92bcd87839528b031557326d28d
Chris Bridewell <cjbaseball5@gmail.com>
2021-06-14 11:50:15 -0400

    Players page css stuff

--- public/src/views/Players/index.vue
+++ public/src/views/Players/index.vue
@@ -2,27 +2,24 @@
   <div class="players-container">
 
     <h1 class="site-title">Players</h1>
-
-    <div class="players-container_content white_card_background">
-      <div class="content-section players-container_content_overall-stats">
-        <grid-table
-          class="league-container_content_overall-stats_table"
-          :columns="overallStatsColumns"
-          :rows="splicedRows"
-          :rowsCount="statsRows ? statsRows.length : 0"
-          :hasHeader="overallStatsColumns.length > 0"
-          :label="'Overall Stats'"
-          :hasPagination="hasPagination"
-          :hasSizeSelector="hasSizeSelector"
-          :pageIndex="pageIndex"
-          :pageSize="pageSize"
-          :paginationRefresh="paginationRefresh"
-          @page-index-change="changePageIndex($event)"
-          @page-size-change="changePageSize($event)"
-          :canHideContent="false"
-          @row-clicked="playerClick"
-        ></grid-table>
-      </div>
+    <div class="players-table white_card_background ">
+      <grid-table
+        class="players-table_table"
+        :columns="overallStatsColumns"
+        :rows="splicedRows"
+        :rowsCount="statsRows ? statsRows.length : 0"
+        :hasHeader="overallStatsColumns.length > 0"
+        :label="'Overall Stats'"
+        :hasPagination="hasPagination"
+        :hasSizeSelector="hasSizeSelector"
+        :pageIndex="pageIndex"
+        :pageSize="pageSize"
+        :paginationRefresh="paginationRefresh"
+        @page-index-change="changePageIndex($event)"
+        @page-size-change="changePageSize($event)"
+        :canHideContent="false"
+        @row-clicked="playerClick"
+      ></grid-table>
     </div>
 
   </div>
