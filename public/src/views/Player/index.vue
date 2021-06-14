e71a538f9479ad21e1eb1864307447fef54d457a
Chris Bridewell <cjbaseball5@gmail.com>
2021-06-14 12:51:49 -0400

    Player page made n stuff

--- public/src/views/Player/index.vue
+++ public/src/views/Player/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="player-container">
-    <h1>Player</h1>
+  <div class="player">
+    <div class="player-stats-table white_card_background ">
+
+      <h1 v-if="player.firstname" class="player-stats-table_firstname">{{player.firstname}}</h1>
+      <p v-if="player.nickname" class="player-stats-table_nickname">
+        <span>a.k.a</span> <br> {{player.nickname}}
+      </p>
+
+      <grid-table
+        class="player-stats-table_table"
+        :columns="columns"
+        :rows="row"
+        :rowsCount="1"
+        :hasHeader="columns.length > 0"
+        :label="'Overall Stats'"
+        :hasPagination="false"
+        :hasSizeSelector="false"
+      ></grid-table>
+      <button class="btn red_btn" @click="contact()">Contact</button>
+      <button class="btn red_btn" @click="inviteToLeague()">Invite to League</button>
+    </div>
+
   </div>
 </template>
 
--- public/src/views/Player/player.ts
+++ public/src/views/Player/player.ts
@@ -1,5 +1,59 @@
-import { defineComponent } from "@vue/runtime-core";
+import { defineComponent } from "@vue/runtime-core"
+import GridTable from '@/components/tables/grid-table/index.vue'
+import { mapActions, mapGetters } from "vuex"
+import contact from "../Contact/contact"
 
 export default defineComponent({
-  name: 'player'
+  name: 'profile',
+  components: {
+    GridTable
+  },
+  data() {
+    return {
+      playerID: "",
+      player: Object(),
+      columns: [],
+      leagueColumns: ['Name', 'Points', 'Placement', 'Record']
+    }
+  },
+  computed: {
+    ...mapGetters(['getIsLoggedIn', 'getLoggedInPlayer']),
+    stats(): any {
+      if (!this.player) return {}
+      const stats: { [key: string]: any } = {}
+
+      if (this.player.player_stats) {
+        Object.keys(this.player.player_stats).forEach((s: string) => {
+          stats[s] = { text: this.player.player_stats[s], type: 'numeric' } 
+        })
+      }
+      return stats
+    },
+    row(): Array<Object> {
+      if (!this.stats) return []
+      return [ this.stats ]
+    }
+  },
+  async created() {
+    this.playerID = String(this.$route.params.id)
+    this.player = await this.fetchPlayerById(this.playerID)
+    this.columns = await this.fetchPlayerStatsTableColumns()
+  },
+  methods: {
+    ...mapActions(['fetchPlayerStatsTableColumns', 'updateUserSettings', 'fetchPlayerById']),
+    redirect(link: string) {
+      if (link == "top") {
+        window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+      }
+      else {
+        this.$router.push(link)
+      }
+    },
+    contact() {
+      alert("To contact this person go talk to them u fkin idiot")
+    },
+    inviteToLeague() {
+      alert("To invite this person to a league go talk to them u fkin idiot")
+    }
+  }
 })
