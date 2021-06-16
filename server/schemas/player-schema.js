08c317d07b58a1b4179679c234316c22cb766493
Devin Harris <dharris@cincom.com>
2021-06-15 20:20:23 -0400

    notifications update

--- server/schemas/player-schema.js
+++ server/schemas/player-schema.js
@@ -16,9 +16,14 @@ const player_schema = new Schema({
   show_information: Boolean,
   league_ids: [Schema.Types.ObjectId],
   token_version: Number,
-  notifications: [
-    notification
-  ],
+  notifications: {
+    league_invitations: [ notification ],
+    league_updates: [ notification ],
+    league_join_requests: [ notification ],
+    contact_request: [ notification ],
+    general_update: [ notification ],
+    other: [ notification ]
+  },
   selected_league_schedules: Array
 }, {strict: false})
 
