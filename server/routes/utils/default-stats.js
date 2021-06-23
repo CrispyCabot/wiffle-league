e8ac8e57b89c04a80d18c80ca7e3b999e883dd71
Devin Harris <dharris@cincom.com>
2021-06-23 10:47:22 -0400

    fixed some styling on toast and game-summary and moved default stats into a util

---
+++ server/routes/utils/default-stats.js
@@ -0,0 +1,13 @@
+module.exports = {
+  hits: 0,
+  singles: 0,
+  doubles: 0,
+  triples: 0,
+  homeruns: 0,
+  plate_appearances: 0,
+  at_bats: 0,
+  games: 0,
+  wins: 0,
+  losses: 0,
+  points: 0
+}
