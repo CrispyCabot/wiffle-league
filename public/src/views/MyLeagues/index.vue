6ddb8137372515f009b0d2af8aae013d55bbfa2d
Chris Bridewell <cjbaseball5@gmail.com>
2021-06-24 11:23:11 -0400

    My leagues view complete

--- public/src/views/MyLeagues/index.vue
+++ public/src/views/MyLeagues/index.vue
@@ -1,6 +1,49 @@
 <template>
-  <div class="my-leagues">
-    <h1>My Leagues</h1>
+  <div class="leagues-container">
+    <h1 class="site-title">My Leagues</h1>
+    <div v-if="!getIsLoggedIn" class="white_card_background">
+      <h3 class="not-logged-in" @click="redirect({redirect: '/login'})">Login to view your leagues</h3>
+    </div>
+      
+    <div v-if="getIsLoggedIn" class="leagues-table white_card_background ">
+
+      <div class="leagues-table_navigation">
+        <radio-slider class="leagues-table_navigation_radio" :values="displayValues" :selectedValue="selectedDisplayValue" @value-change="displayViewChange" />
+      </div>
+
+      <grid-table
+        v-if="selectedDisplayValue == 'Detailed' && columns.length > 0"
+        class="leagues-table_table"
+        :columns="columns"
+        :rows="splicedRows"
+        :rowsCount="rows.length"
+        :hasHeader="true"
+        :hasPagination="hasPagination"
+        :hasSizeSelector="hasSizeSelector"
+        :pageIndex="pageIndex"
+        :pageSize="pageSize"
+        :paginationRefresh="paginationRefresh"
+        @page-index-change="changePageIndex($event)"
+        @page-size-change="changePageSize($event)"
+        @row-clicked="handleLeagueClick"
+      ></grid-table>
+
+      <div v-if="selectedDisplayValue == 'Simple' && columns.length > 0" class="leagues-table_cards">
+        <row-card v-for="row in splicedRows" :key="row.id.text" :row="row" :columns="columns" @row-click="handleLeagueClick" />
+        <pagination
+          class="leagues-table_cards_pagination"
+          :totalItemCount="rows.length"
+          :pageIndex="pageIndex"
+          :pageSize="pageSize"
+          :hasSizeSelector="hasSizeSelector"
+          :paginationRefresh="paginationRefresh"
+          @page-index-change="changePageIndex($event)"
+          @page-size-change="changePageSize($event)"
+        />
+      </div>
+
+      <button class="leagues-table_btn btn red_btn" @click="$router.push('/leagues/schedules')">View schedules</button>
+    </div>
   </div>
 </template>
 
--- public/src/views/MyLeagues/my-leagues.ts
+++ public/src/views/MyLeagues/my-leagues.ts
@@ -1,5 +1,92 @@
 import { defineComponent } from "@vue/runtime-core";
+import { mapActions, mapGetters, mapMutations } from "vuex"
+import SearchInput from '@/components/inputs/search-input/index.vue'
+import RadioSlider from '@/components/inputs/radio-slider/index.vue'
+import GridTable from '@/components/tables/grid-table/index.vue'
+import RowCard from '@/components/cards/row-card/index.vue'
+import Pagination from '@/components/navigation/pagination/index.vue'
+import PaginationMixin from '@/mixins/pagination-mixin'
+import Breadcrumb from '@/components/navigation/breadcrumb/index.vue'
 
 export default defineComponent({
-  name: 'my-leagues'
+  name: 'leagues',
+  components: {
+    SearchInput,
+    RadioSlider,
+    GridTable,
+    RowCard,
+    Pagination,
+    Breadcrumb
+  },
+  mixins: [PaginationMixin],
+  data() {
+    return {
+      allLeagues: [],
+      playerID: "",
+      leagues: [],
+      searchValue: '',
+      displayValues: ['Detailed', 'Simple'],
+      selectedDisplayValue: 'Simple',
+      columns: [],
+      paginationRefresh: true
+    }
+  },
+  computed: {
+    ...mapGetters(['getIsLoggedIn', 'getLoggedInPlayer']),
+    rows(): Array<Object> {
+      return this.leagues
+        .map((league: any) => {
+          const simple_row = {
+            name: {text: league.name, subtitle: '', type: 'title'},
+            players: {text: league.player_ids.length + ' / ' + league.max_num_players, type: 'numeric'},
+            id: {text: league._id, type: 'hidden'}
+          }
+          if (this.selectedDisplayValue == 'Simple') return simple_row
+          
+          return {
+            ...simple_row,
+            name: {text: league.name, type: 'string'},
+            startDate: {text: new Date(league.start_date).toLocaleDateString(undefined, {year: 'numeric', month: 'numeric', day: 'numeric'}), type: 'date'},
+            endDate: {text: new Date(league.end_date).toLocaleDateString(undefined, {year: 'numeric', month: 'numeric', day: 'numeric'}), type: 'date'},
+          }
+        })
+    },
+    splicedRows(): Array<Object> {
+      return this.rows.slice(this.startingIndex, this.endingIndex)
+    }
+  },
+  async created() {
+    this.allLeagues = await this.fetchLeagues()
+    const pid = this.getLoggedInPlayer._id
+    const playerOwnedLeagues: any = []
+    this.allLeagues.forEach(function(val: any, index: number) {
+      if (val.league_creator_id == pid)
+        playerOwnedLeagues.push(val)
+    })
+    this.leagues = playerOwnedLeagues
+
+    this.columns = await this.fetchLeaguesTableColumns()
+  },
+  methods: {
+    ...mapActions(['fetchLeagues', 'fetchLeaguesTableColumns']),
+    searchValueChange(value: string) {
+      this.searchValue = value
+    },
+    displayViewChange(view: string) {
+      this.selectedDisplayValue = view
+    },
+    handleLeagueClick(row: any) {
+      const id = row.id.text
+      this.$router.push(`/league/${id}`)
+    },
+    redirect(link: any) {
+      console.log(link)
+      this.$router.push(link.redirect)
+    }
+  },
+  watch: {
+    rows() {
+      this.paginationRefresh = !this.paginationRefresh
+    }
+  }
 })
