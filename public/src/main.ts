4ef1efc1e89e751f5c36c8cbbbda8b807f1aeed7
Chris Bridewell <cjbaseball5@gmail.com>
2022-02-21 11:39:25 -0500

    test

--- public/src/main.ts
+++ public/src/main.ts
@@ -1,21 +1,21 @@
-import { createApp } from 'vue'
-import App from './app.vue'
-import router from './router'
-import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas  } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import VueClickAway from "vue3-click-away"
-const InlineSvg = require('vue-inline-svg')
+import { createApp } from "vue";
+import App from "./app.vue";
+import router from "./router";
+import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import VueClickAway from "vue3-click-away";
+const InlineSvg = require("vue-inline-svg");
 
-library.add(fas)
-library.add(fab)
+library.add(fas);
+library.add(fab);
 
 createApp(App)
-  .component('font-awesome-icon', FontAwesomeIcon)
-  .component('inline-svg', InlineSvg)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .component("inline-svg", InlineSvg)
   .use(store)
   .use(router)
   .use(VueClickAway)
-  .mount('#app')
+  .mount("#app");
