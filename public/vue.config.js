2f82448e794c3b82241ad54734ea14dbf38644e5
Chris Bridewell <cjbaseball5@gmail.com>
2022-02-21 11:50:49 -0500

    deploying live site

--- public/vue.config.js
+++ public/vue.config.js
@@ -1,12 +1,15 @@
+const path = require("path");
+
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
           @import "~@/scss/_colors.scss";
           @import "~@/scss/_fonts.scss";
-        `
-      }
-    }
-  }
-}
+        `,
+            },
+        },
+    },
+    outputDir: path.resolve(__dirname, "../deploy"),
+};
